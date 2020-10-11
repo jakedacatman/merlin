5cf6e74068188e82dc66d703b54a2a8a24f54803
jakedacatman <helloihaxxedu@gmail.com>
2020-10-11 12:14:03 -0400

    push changes

--- services/audioservice.cs
+++ services/audioservice.cs
@@ -26,84 +26,80 @@ namespace donniebot.services
         public async Task<bool> ConnectAsync(SocketVoiceChannel channel)
         {
             var id = channel.Guild.Id;
+
+            await channel.ConnectAsync(true);
+            await channel.DisconnectAsync();
+            
             var connection = await channel.ConnectAsync(true, false, true);
 
             if (!_connections.ContainsKey(id))
                 return _connections.TryAdd(channel.Guild.Id, connection);
-            else if (!_connections.TryGetValue(id, out var comp))
-                return false;
             else
-                return _connections.TryUpdate(id, connection, comp);
+                return (_connections.TryRemove(id, out var _) && _connections.TryAdd(id, connection));
         }
 
         public async Task<bool> DisconnectAsync(SocketVoiceChannel channel)
         {
             var id = channel.Guild.Id;
 
+            await channel.DisconnectAsync();
+
             if (!_connections.TryGetValue(id, out var connection))
                 return false;
-            
-            await channel.DisconnectAsync();
             connection.Dispose();
 
             return _connections.TryRemove(id, out var _);
         }
 
+
+        public async Task PlayAsync(SocketVoiceChannel channel, string url)
+        {
+            await ConnectAsync(channel);
+            await PlayAsync(channel.Guild.Id, url);
+        }
         public async Task PlayAsync(ulong id, string url)
         {
             try
             {
-                if (!_connections.TryGetValue(id, out IAudioClient connection))
+                if (!_connections.TryGetValue(id, out var connection))
                     throw new InvalidOperationException("Not connected to a voice channel.");
 
-                Console.WriteLine(connection);
                 var audioUrl = GetUrl(url);
 
                 var req = WebRequest.Create(audioUrl);
-                req.BeginGetResponse(async result =>
+
+                using (var response = await req.GetResponseAsync())
+                using (var str = response.GetResponseStream())
+
+                using (var ffmpeg = CreateStream())
+                using (var output = ffmpeg.StandardOutput.BaseStream)
+                using (var input = ffmpeg.StandardInput.BaseStream)
+
+                using (var discord = connection.CreatePCMStream(AudioApplication.Mixed))
                 {
-                    var handle = result.AsyncWaitHandle;
-                    handle.WaitOne();
-                
-                    var res = (WebRequest)result.AsyncState;
-
-                    using (var response = res.EndGetResponse(result))
-                    using (var str = response.GetResponseStream())
+                    try
                     {
-                        using (var ffmpeg = CreateStream())
-                        using (var output = ffmpeg.StandardOutput.BaseStream)
-                        using (var input = ffmpeg.StandardInput.BaseStream)
-                        using (var discord = connection.CreatePCMStream(AudioApplication.Mixed))
-                        {
-                            try
-                            {
-                                var buffer = new byte[4096];
-                                var bytesRead = 0;
-                                var bytesConverted = 0;
+                        var buffer = new byte[4096];
+                        var bytesRead = 0;
+                        var bytesConverted = 0;
 
-                                while (str.Position <= str.Length)
-                                {
-                                    bytesRead += await str.ReadAsync(buffer, bytesRead, 4096);
-                                    await input.WriteAsync(buffer, bytesRead, 4096);
-                                    bytesConverted += await output.ReadAsync(buffer, bytesRead, 4096);
-                                    await discord.ReadAsync(buffer, bytesConverted, 4096);
-                                }
-                            }
-                            finally 
-                            {
-                                await discord.FlushAsync();
-                            }
+                        while (str.Position <= str.Length)
+                        {
+                            bytesRead += await str.ReadAsync(buffer, bytesRead, 4096);
+                            await input.WriteAsync(buffer, bytesRead, 4096);
+                            bytesConverted += await output.ReadAsync(buffer, bytesConverted, 4096);
+                            await discord.ReadAsync(buffer, bytesConverted, 4096);
                         }
                     }
-
-                    return;
-                }, req);
-
-                await Task.Delay(-1);
+                    finally 
+                    {
+                        await discord.FlushAsync();
+                    }
+                }
             }
             catch (Exception e)
             {
-                throw e;
+                Console.WriteLine(e);
             }
         }
 
@@ -133,9 +129,10 @@ namespace donniebot.services
             proc.Start();
             proc.BeginOutputReadLine();
             proc.WaitForExit(); //blocks :( (good thing we run in RunMode.Async)
-
-            var url = urls.LastOrDefault();
-            return url; //last one (audio)
+            
+            var query = urls.Where(x => x.Contains("mime=audio"));
+            var url = query.Any() ? query.First() : urls.LastOrDefault();
+            return url;
         }
 
         private Process CreateStream()
