fda06f4a4d243f52a67d1f86f560b64eca3bb0df
jakedacatman <helloihaxxedu@gmail.com>
2020-10-11 00:01:59 -0400

    connection is null or something

--- services/audioservice.cs
+++ services/audioservice.cs
@@ -1,10 +1,14 @@
 using System;
 using Discord;
+using System.IO;
+using System.Net;
+using System.Linq;
 using Discord.Audio;
 using Discord.WebSocket;
-using System.Threading.Tasks;
-using System.Collections.Concurrent;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace donniebot.services
 {
@@ -47,29 +51,94 @@ namespace donniebot.services
 
         public async Task PlayAsync(ulong id, string url)
         {
-            if (!_connections.TryGetValue(id, out var connection))
-                throw new InvalidOperationException("Not connected to a voice channel.");
+            try
+            {
+                if (!_connections.TryGetValue(id, out IAudioClient connection))
+                    throw new InvalidOperationException("Not connected to a voice channel.");
 
-            var audioUrl = await GetUrlAsync(url);
+                Console.WriteLine(connection);
+                var audioUrl = GetUrl(url);
+
+                var req = WebRequest.Create(audioUrl);
+                req.BeginGetResponse(async result =>
+                {
+                    var handle = result.AsyncWaitHandle;
+                    handle.WaitOne();
+                
+                    var res = (WebRequest)result.AsyncState;
+
+                    using (var response = res.EndGetResponse(result))
+                    using (var str = response.GetResponseStream())
+                    {
+                        using (var ffmpeg = CreateStream())
+                        using (var output = ffmpeg.StandardOutput.BaseStream)
+                        using (var input = ffmpeg.StandardInput.BaseStream)
+                        using (var discord = connection.CreatePCMStream(AudioApplication.Mixed))
+                        {
+                            try
+                            {
+                                var buffer = new byte[4096];
+                                var bytesRead = 0;
+                                var bytesConverted = 0;
+
+                                while (str.Position <= str.Length)
+                                {
+                                    bytesRead += await str.ReadAsync(buffer, bytesRead, 4096);
+                                    await input.WriteAsync(buffer, bytesRead, 4096);
+                                    bytesConverted += await output.ReadAsync(buffer, bytesRead, 4096);
+                                    await discord.ReadAsync(buffer, bytesConverted, 4096);
+                                }
+                            }
+                            finally 
+                            {
+                                await discord.FlushAsync();
+                            }
+                        }
+                    }
+
+                    return;
+                }, req);
+
+                await Task.Delay(-1);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
-        public async Task<string> GetUrlAsync(string ytUrl)
+        public string GetUrl(string ytUrl)
         {
-            var proc = Process.Start(new ProcessStartInfo
+
+            var info = new ProcessStartInfo
             {
                 FileName = "youtube-dl",
-                Arguments = $"--no-cache-dir -g",
+                Arguments = $"--no-cache-dir -g {ytUrl}",
                 UseShellExecute = false,
                 RedirectStandardOutput = true
+            };
+            var proc = new Process
+            {
+                EnableRaisingEvents = true,
+                StartInfo = info
+            };
+
+            var urls = new List<string>();
+            proc.OutputDataReceived += new DataReceivedEventHandler((s, e) =>
+            {
+                if (!string.IsNullOrWhiteSpace(e.Data))
+                    urls.Add(e.Data);
             });
 
-            var lines = (await proc.StandardOutput.ReadToEndAsync()).Split('\n');
-            var url = lines[lines.Length - 1];
-            Console.WriteLine(url);
+            proc.Start();
+            proc.BeginOutputReadLine();
+            proc.WaitForExit(); //blocks :( (good thing we run in RunMode.Async)
+
+            var url = urls.LastOrDefault();
             return url; //last one (audio)
         }
 
-        private Process CreateStream(string path)
+        private Process CreateStream()
         {
             return Process.Start(new ProcessStartInfo
             {
