02d570335bbd17dc05eb7e25e5a5c796b2f2234f
jakedacatman <helloihaxxedu@gmail.com>
2020-12-17 23:36:09 -0500

    everything should be fine now

---
+++ src/classes/images/svgformat.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using SixLabors.ImageSharp.Formats;
+
+namespace donniebot.classes
+{
+    public class SvgFormat : IImageFormat
+    {
+        public string Name => "SVG";
+        public string DefaultMimeType => "image/svg+xml";
+        public IEnumerable<string> MimeTypes => new[] { "image/svg+xml" };
+        public IEnumerable<string> FileExtensions => new[] { "svg" };
+    }
+}
