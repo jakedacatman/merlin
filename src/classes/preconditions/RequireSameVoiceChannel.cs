fd9d066718757ee3fe49588fec7d5def945e8d93
jakedacatman <helloihaxxedu@gmail.com>
2021-10-15 19:08:41 -0400

    fix same voice channel perms

--- src/classes/preconditions/RequireSameVoiceChannel.cs
+++ src/classes/preconditions/RequireSameVoiceChannel.cs
@@ -15,7 +15,11 @@ namespace donniebot.classes
         public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
         {
             var currUser = await context.Guild.GetUserAsync(context.Client.CurrentUser.Id);
-            if (currUser.VoiceChannel is not null && (context.User as SocketGuildUser).VoiceChannel == currUser.VoiceChannel) 
+            var commandUser = context.User as SocketGuildUser;
+            if (currUser.VoiceChannel is not null && commandUser.VoiceChannel == currUser.VoiceChannel) 
+                return PreconditionResult.FromSuccess();
+            
+            else if (currUser.VoiceChannel is null && commandUser.VoiceChannel is not null)
                 return PreconditionResult.FromSuccess();
 
             return PreconditionResult.FromError("You must be in the same voice channel as me.");
