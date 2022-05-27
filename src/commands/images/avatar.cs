daa74fe2d499a1929c72d19532d6e9d58972ba60
jakedacatman <helloihaxxedu@gmail.com>
2022-05-27 14:26:08 -0400

    made avatar command work with roblox avatars

--- src/commands/images/avatar.cs
+++ src/commands/images/avatar.cs
@@ -25,6 +25,7 @@ namespace merlin.commands
 
         [Command("avatar")]
         [Alias("a", "av")]
+        [Priority(0)]
         [Summary("Gets a user's avatar.")]
         public async Task AvatarAsync([Summary("The user.")] SocketGuildUser user = null)
         {
@@ -56,6 +57,21 @@ namespace merlin.commands
                 .WithColor(_rand.RandomColor())
                 .WithImageUrl(url)
                 .WithTitle($"Avatar for {userId}")
+                .WithUrl(url)
+                .WithCurrentTimestamp()
+                .Build(), messageReference: new MessageReference(Context.Message.Id), allowedMentions: AllowedMentions.None);
+        }
+        [Command("avatar")]
+        [Alias("a", "av")]
+        [Priority(0)]
+        [Summary("Gets a user's avatar.")]
+        public async Task AvatarAsync([Summary("The ROBLOX username (put \"roblox:\" in front).")] string user)
+        {
+            var url = await _img.ParseUrlAsync(user, Context.Message, true);
+            await ReplyAsync(embed: new EmbedBuilder()
+                .WithColor(_rand.RandomColor())
+                .WithImageUrl(url)
+                .WithTitle($"Avatar for {user.Substring(7)}")
                 .WithUrl(url)
                 .WithCurrentTimestamp()
                 .Build(), messageReference: new MessageReference(Context.Message.Id), allowedMentions: AllowedMentions.None);
